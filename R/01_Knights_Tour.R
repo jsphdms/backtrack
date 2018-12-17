60b0e52fcd061ac9b3015b79f786ab851c311cf9
Joseph Adams <joseph@josephadams.net>
2018-12-17 22:22:01 +0000

    Create function to check validity of move

--- R/01_Knights_Tour.R
+++ R/01_Knights_Tour.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,14 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+kt_valid_move <- function(i, j, solution) {
+
+  valid <- all(0 < i,
+               0 < j,
+               i < nrow(solution) + 1,
+               j < nrow(solution) + 1,
+               is.na(solution[i, j]))
+
+  return(valid)
+
 }
